af8a9f56188032b8c5c9985d0dade1841b56e824
developer36chirag-create <developer36chirag@gmail.com>
2026-05-12 16:56:24 +0530

    added about, tree trimming, tree removal pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 
 import Home from './pages/Home';
 import Services from './pages/Services';
-import TreeTrimming from './pages/TreeTrimming';
+import TreeTrimming from './pages/Treetrimming';
 import Treeremoval from './pages/Treeremoval';
 import About from './pages/About';
 
