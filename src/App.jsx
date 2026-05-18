1fcb470f4c1490ed16fd4b5bf5816884fd3c2d58
developer36chirag-create <developer36chirag@gmail.com>
2026-05-18 18:06:27 +0530

    add single city pages

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,6 @@ import Contact from './pages/Contact';
 import Citypage from './pages/Citypage';
 import Alpharetta from './pages/Alpharetta';
 import Atlanta from './pages/Atlanta';
-import Avondaleestates from './pages/Avondaleestates';
 import Buford from './pages/Buford';
 import Cumming from './pages/Cumming';
 import Decatur from './pages/Decatur';
@@ -46,7 +45,6 @@ export default function App() {
         <Route path="/contact-us" element={<Contact />} />
         <Route path="/alpharetta" element={<Alpharetta />} />
         <Route path="/atlanta" element={<Atlanta />} />
-        <Route path="/avondale-estates" element={<Avondaleestates />} />
         <Route path="/buford" element={<Buford />} />
         <Route path="/cumming" element={<Cumming />} />
         <Route path="/decatur" element={<Decatur />} />
